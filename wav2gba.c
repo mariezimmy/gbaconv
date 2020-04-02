a8df1eed3d1e86e117a0a844002e93a7fce70b5a
mariezimmy <mariezimmy@users.noreply.github.com>
2020-04-02 10:23:55 -0400

    fork to create .h and .c files for sound array

--- wav2gba.c
+++ wav2gba.c
@@ -1,28 +1,24 @@
 /*
- * GBAconv 1.00
- * Copyright (c) 2002-2019, Frederic Cambus
- * https://github.com/fcambus/gbaconv
- *
- * WAV to GBA Converter
- *
- * Created:      2002-12-10
- * Last Updated: 2019-05-09
- *
- * GBAconv is released under the BSD 2-Clause license.
- * See LICENSE file for details.
+ * Forked from https://github.com/fcambus/gbaconv
+ * Includes writing .c and .h files with sound array information
+ * to the directory where the provided .wav resides
  */
 
+#include <ctype.h> 
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <unistd.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <unistd.h>  
 
 #define WAVE_HEADER_LENGTH 44
 
 char *input_file_buffer;
+
 
 struct wave_header {
 	char chunk_ID[4];
@@ -44,6 +40,38 @@ int main(int argc, char *argv[])
 {
 	int fd;
 	struct stat st;
+
+	// capitalize the input name
+	char* name = malloc(strlen(argv[2]));
+	strcat(name, argv[2]);
+	int i = 0;
+	while(name[i]) {
+		name[i] = toupper(name[i]);
+		i++;
+	}
+
+	// create .c file name from input
+	char* cFile = malloc(strlen(argv[1]) - 2);
+	char* cDirName = malloc(strlen(argv[1]) - 4);
+	strncpy(cDirName, argv[1], strlen(argv[1]) - 4);
+	strcat(cFile, cDirName);
+	strcat(cFile, ".c");
+
+	// create .h file name from input
+	char* hFile = malloc(strlen(argv[2]) - 2);
+	char* hDirName = malloc(strlen(argv[1]) - 4);
+	strncpy(hDirName, argv[1], strlen(argv[1]) - 4);
+	strcat(hFile, cDirName);
+	strcat(hFile, ".h");
+
+	// start .c file
+	FILE *foutc = fopen(cFile, "w");
+
+	// start .h file
+	FILE *fouth = fopen(hFile, "w");
+	fprintf(fouth, "#ifndef __%sH__\n", name);
+	fprintf(fouth, "#define __%sH__\n\n", name);
+
 
 	if (argc != 3) {
 		printf("USAGE: wav2gba input.wav array_name (Input File must be 8-bit, MONO)\n\n");
@@ -84,18 +112,36 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	fprintf(stderr, "INPUT  FILE: %s (8-bit, MONO, %i Hz)\n", argv[1], wave_header.sample_rate);
+	// .h len macro and extern array
+	fprintf(fouth, "#define %sLEN %llu\n", name, st.st_size - WAVE_HEADER_LENGTH);
+	fprintf(fouth, "extern const signed char %s[%llu];\n\n", argv[2], st.st_size - WAVE_HEADER_LENGTH);
 
-	fprintf(stdout, "const s8 %s[] = {", argv[2]);
-
+	// assign array in .c
+	fprintf(foutc, "const signed char %s[%llu] = {", argv[2], st.st_size - WAVE_HEADER_LENGTH);
 	for (size_t loop = 0; loop < st.st_size - WAVE_HEADER_LENGTH; loop++) {
-		if (loop % 10 == 0)
-			fprintf(stdout, "\n\t");
+		if (loop % 10 == 0) {
+			fprintf(foutc, "\n\t");
+		}
+		
+		signed char value = (signed char) input_file_buffer[WAVE_HEADER_LENGTH + loop] + 128;
+		
+		// probably unnecessary
+		if (value < -128) {
+			value = -128;
+		} else if (value > 127) {
+			value = 127;
+		}
 
-		fprintf(stdout, "0x%x,", input_file_buffer[WAVE_HEADER_LENGTH + loop] + 128);
+		if (loop == st.st_size - WAVE_HEADER_LENGTH - 1) {
+			fprintf(foutc, "%d", ((signed char) value));
+		} else {
+			fprintf(foutc, "%d,", ((signed char) value));
+		}
 	}
 
-	fprintf(stdout, "\n};\n");
+	fprintf(foutc, "\n};\n");
+	fprintf(fouth, "#endif");
+
 
 	/* Terminate Program */
 	munmap(input_file_buffer, st.st_size);
